65662d41d0b7a035cdc684e145e7a49947c7a155
YASUHISA <YASUHISA@IP-C613B678>
2019-10-21 22:09:30 +0900

    プロジェクト ファイルを追加します。

---
+++ Index.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HttpRequestMocking
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        private readonly IndexController  _controller = new IndexController();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            _controller.createCookie(new HttpRequestWrapper(Request),
+                                                     new HttpResponseWrapper(Response));
+        }
+    }
+}
---
+++ indexController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HttpRequestMocking
+{
+    public class IndexController
+    {
+        public IndexController()
+        {
+
+        }
+
+        public void createCookie(HttpRequestBase request, HttpResponseBase response)
+        {
+            if (request.QueryString == null)
+            {
+                throw new ArgumentNullException();
+            }
+            string value = request.QueryString["hoge"];
+
+            var cookie = new HttpCookie("hoge");
+            cookie.Value = "hoge" + value;
+            response.Cookies.Add(cookie);
+        }
+
+    }
+}
